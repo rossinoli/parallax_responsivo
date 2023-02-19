6533a6ccacc090339b34c57d358e5767f645b6fb
thaina rossinoli <rossinoli.2001@gmail.com>
2023-02-19 09:07:45 -0300

    parallaxdeblackclover

---
+++ script.js
@@ -0,0 +1,15 @@
+/* sbv = confirm("você está gostando da pagina?");
+if(sbv == true ){
+    alert("Ficamos imensamente feliz que você esteja gostando!");}
+else{
+    alert("poxa, Em que podemos melhorar");
+} */
+
+/*var nomeusuario = prompt("Qual seu nome?, nome");
+if(nomeusuario !=""){
+    alert("Seu nome é: "+nomeusuario+"!");
+}else{
+alert("Poxa nem sei qual é seu nome =/");
+}
+*/
+
